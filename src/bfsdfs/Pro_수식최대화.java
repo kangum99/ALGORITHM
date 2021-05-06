82bab5f6f037fcc1698e957499ff4d6ded08ebef
yuum <kangum99@naver.com>
2021-05-06 19:08:58 +0900

    카카오인턴2020

--- src/bfsdfs/Pro_수식최대화.java
+++ src/bfsdfs/Pro_수식최대화.java
@@ -4,24 +4,28 @@ import java.util.ArrayList;
 public class Pro_수식최대화 {
 //	수식을 넣기
 	static ArrayList<Long> list = new ArrayList<Long>();
-//	나온연산자들을 넣기
 	static ArrayList<String> oper = new ArrayList<String>();
 	static ArrayList<String> op = new ArrayList<String>();
+//	우선순위에 맞는 순열 넣을 배열
 	static String[] pre = new String[3];
 	static boolean[] visit = new boolean[3];
 	static long max = 0; 
 	
 	static long solution(String expression) {
+//		식 나누기
         divString(expression);
+
         dfs(0, op.size());
         return max;
     }
 	static void dfs(int index, int r) {
 		if(index == r ) {
+//			(우선순위)에 맞게 계산
 			sol();
 			return;
 		}else {
 			for(int i = 0; i < r; i++) {
+//				op순열 (우선순위)
 				if(!visit[i]) {
 					visit[i] = true;
 					pre[index] = op.get(i);
@@ -32,16 +36,19 @@ public class Pro_수식최대화 {
 		}
 	}
 	static void sol() {
+//		계산을 위한 copy
 		ArrayList<Long> num = new ArrayList<Long>();
 		num.addAll(list);
 		ArrayList<String> operation = new ArrayList<String>();
 		operation.addAll(oper);
 		
+//		우선순위에 맞게 계산
 		for(int i = 0; i < pre.length; i++) {
 			for(int j = 0; j < operation.size(); j++) {
 				if(pre[i].equals(operation.get(j))) {
 					long n = num.get(j);
 					long m = num.get(j+1);
+//					사칙연산
 					long ans = cal(n, m, operation.get(j));
 					
 					num.remove(j+1);
@@ -72,6 +79,7 @@ public class Pro_수식최대화 {
 	
 	static void divString (String expression) {
 		int point = 0;
+//		들어있는 연산들의 배열 : op
 		if (expression.contains("+")) {
         	op.add("+");
 		}
@@ -81,6 +89,7 @@ public class Pro_수식최대화 {
         if (expression.contains("*")) {
         	op.add("*");
         }
+//        숫자, 연산자 배열 : list, oper
         for(int i = 0; i < expression.length(); i++) {
         	Character c = expression.charAt(i);
         	
